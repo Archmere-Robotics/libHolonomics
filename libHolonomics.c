8a9ec32085efd4cf72ed48f9ca912921d0153dd6
mailmindlin <client>
2015-01-14 16:19:59 -0500

    Merge branch 'beta'
    Conflicts:
    	libHolonomics.c
    Functions addVal and normalize will be missed.

--- libHolonomics.c
+++ libHolonomics.c
@@ -13,17 +13,15 @@ It controlls the holonomic drive system for '
 */
 
 //internal variable declarations/definitions
-static int ra, rb, rc, rd=0;//motor values.
 //stops everything
 void Stop(){
 	motor[wheelA]  = 0;
 	motor[wheelB]  = 0;
 	motor[wheelC]  = 0;
 	motor[wheelD]  = 0;
-	ra             = 0;
-	rb             = 0;
-	rc             = 0;
-	rd             = 0;
+	vecX		=0;
+	vecY		=0;
+	vecZ		=0;
 	return;
 }
 
@@ -37,42 +35,17 @@ void cDir(int wA, int wB, int wC, int wD) {
 }
 
 //for testing: rotates in place
-#if defined(USING_rotate) || defined(HOLO_DEBUG)
-void rotate(int p) {
-	cDir(p,p,p,p);
-}
-#endif
+#define rotate(p) cDir(p,p,p,p)
 
-void addVal(int wA, int wB, int wC, int wD) {
-	ra = ra + wA;
-	rb = rb + wB;
-	rc = rc + wC;
-	rd = rd + wD;
-}
-
-//fixes motor values
-int normalize(int mod2) {
-	if(abs(mod2)<MOTOR_MAX)return mod2;
-	float mod = MOTOR_MAX/((float)abs(mod2));
-	ra *= mod;
-	rb *= mod;
-	rc *= mod;
-	rd *= mod;
-	return mod2>0?MOTOR_MAX:-MOTOR_MAX;
-}
-#ifdef AWD
+#if defined(AWD)
 void applyAWD() {
-	ra+=rotationOffset-vecX-vecY;
-	rb+=rotationOffset+vecX-vecY;
-	rc+=rotationOffset+vecX+vecY;
-	rd+=rotationOffset-vecX+vecY;
+	vecX+=offX;
+	vecY+=offY;
+	vecZ+=offZ;
 	//now, update offset/vector
 	int dRotation=nMotorEncoder[wheelA]+nMotorEncoder[wheelB]+nMotorEncoder[wheelC]+nMotorEncoder[wheelD];
 	int dx=-nMotorEncoder[wheelA]+nMotorEncoder[wheelB]+nMotorEncoder[wheelC]-nMotorEncoder[wheelD];
 	int dy=-nMotorEncoder[wheelA]-nMotorEncoder[wheelB]+nMotorEncoder[wheelC]+nMotorEncoder[wheelD];
-	int aRotation=ra+rb+rc+rd;
-	int ax=-ra+rb+rc-rd;
-	int ay=-ra-rb+rc+rd;
 	#ifdef DEBUG_AWD
 		bDisplayDiagnostics=false;
 		eraseDisplay();
@@ -92,21 +65,22 @@ void applyAWD() {
 
 //makes stuff happen
 void loadVal() {
-	#ifdef NEW_MOTION
-		//convert vector to motor values
+	#if defined(AWD) && defined(sdfsdf)
+		applyAWD();
 	#endif
-	#ifdef DEBUG_MOTOR_VALUES
-		//store values for display later
-		int oldA=ra,oldB=rb,oldC=rc,oldD=rd;
-	#endif
+	//convert vectors to motor values
+	float ra= vecX+vecY+vecZ;
+	float rb=-vecX+vecY+vecZ;
+	float rc=-vecX-vecY+vecZ;
+	float rd= vecX-vecY+vecZ;
 	//normalize values
-	ra=normalize(ra);
-	rb=normalize(rb);
-	rc=normalize(rc);
-	rd=normalize(rd);
-	#ifdef AWD
-		//applyAWD();
-	#endif
+	float modifier=abs(MOTOR_MAX/maxof4(ra,rb,rc,rd));
+	if(modifier<1.0){
+		ra*=modifier;
+		rb*=modifier;
+		rc*=modifier;
+		rd*=modifier;
+	}
 	//write values to the motors
 	cDir(ra,rb,rc,rd);
 	//now, set other non-drivetrain motors
@@ -115,36 +89,38 @@ void loadVal() {
 	motor[collectorMotor]=conveyorSpeed;
 	#ifdef DEBUG_MOTOR_VALUES
 		//display motor values to screen
-		nxtDisplayCenteredTextLine(1,"A: %3d -> %3d",oldA,ra);
-		nxtDisplayCenteredTextLine(2,"B: %3d -> %3d",oldB,rb);
-		nxtDisplayCenteredTextLine(3,"C: %3d -> %3d",oldC,rc);
-		nxtDisplayCenteredTextLine(4,"D: %3d -> %3d",oldD,rd);
+		float vdx=vecX*50/MOTOR_MAX;
+		float vdy=vecY*32/MOTOR_MAX;
+		float vdz=vecZ*50/MOTOR_MAX;
+		eraseDisplay();
+		bDisplayDiagnostics=false;
+		fillEllipse(46+vdx,28+vdy,54+vdx,36+vdy);
+		drawLine(50+vdz,64,50+vdz,56);
 	#endif
 	//reset internal motor values
-	ra=0;
-	rb=0;
-	rc=0;
-	rd=0;
+	vecX=0.0;
+	vecY=0.0;
+	vecZ=0.0;
 }
 
 //updates servo values. call once per cycle
 void updateServos() {
 	//move dump
-	if(dumpActive){//checks if the lift servo is activated
+	if (dumpActive) {//checks if the lift servo is activated
 		//if so, move lift
 		servoChangeRate[dumpServo]=1;
 		servo[dumpServo]=dumpservo_pos+DUMPSERVO_FLAT;
-	}else
+	} else
 		servoChangeRate[dumpServo]=0;
-	if(rightIRActive) {
+	if (rightIRActive) {
 		servoChangeRate[rightIRServo]=1;
 		servo[rightIRServo]=RIGHT_SERVO_POS;
-	}else
+	} else
 		servoChangeRate[rightIRServo]=1;
-	if(leftIRActive){
+	if (leftIRActive) {
 		servoChangeRate[leftIRServo]=1;
 		servo[leftIRServo]=LEFT_SERVO_POS;
-	}else
+	} else
 		servoChangeRate[leftIRServo]=1;
 	if(liftAuto) {
 		int liftPos=nMotorEncoder[liftMotor];
@@ -163,19 +139,23 @@ void updateServos() {
 	servoChangeRate[rightHook]=5;
 	servo[rightHook]=rightHookPos;
 }
-#if defined(NEW_MOTION)
-void addRotation(int dRotation){
-	rotation+=dRotation;
+void addRotation(int dRotation) {
+	if(dRotation>0)
+		vecZ+=dRotation;
+	else
+		vecZ-=abs(dRotation);
 }
-void addVector(int x, int y){
+void addRotation(float dRotation) {
+	vecZ+=dRotation;
+}
+void addVector(float x, float y) {
 	vecX+=x;
 	vecY+=y;
 }
-void addMovement(int x, int y, int dRotation){
+void addMovement(float x, float y, float dRotation) {
 	addVector(x,y);
 	addRotation(dRotation);
 }
-#endif
 //returns lift position as integer
 #if defined(HOLO_DEBUG) || defined(USING_ALL) || defined(USING_igetLiftSpeed)
 int igetLiftSpeed() {
